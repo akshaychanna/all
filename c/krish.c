ed71c692ace382a8f94f087f239ee04c6fd81aa5
akshaychanna <akshaychanna77@gmail.com>
2020-10-09 23:05:35 +0530

    c

---
+++ c/krish.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+	int start =11;
+	int end = 20;
+	for(start; start<end; start++)
+	{
+		if (start%3==0 && start%5==0)
+                {
+                        printf("akshay krish\n");
+                }
+
+		else if(start%3==0)
+		{
+			printf("akshay\n");
+		}
+		else if(start%5==0)
+		{
+			printf("krish\n");
+		}
+		else 
+		{
+			printf("%d \n",start);
+			printf("\n");
+		}
+	}
+
+}
+
---
+++ c/test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char *a[]) {
+    int x ;
+    x = atoi(a[1]);
+    printf("x is : %d\n", x);
+    return 0;
+}
